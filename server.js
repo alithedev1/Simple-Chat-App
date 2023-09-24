02f39c2a3a84614a7b0ec7081dfa9bcee92c6899
Ali the Dev <105778118+alithedev1@users.noreply.github.com>
2023-09-24 09:53:59 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(cors());
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('set username', (username) => {
+    socket.username = username;
+    io.emit('user joined', username);
+  });
+
+  socket.on('disconnect', () => {
+    if (socket.username) {
+      io.emit('user left', socket.username);
+    }
+  });
+
+  socket.on('chat message', (message) => {
+    io.emit('chat message', { username: socket.username, message });
+  });
+});
+
+server.listen(80, () => {
+  console.log('Server is running on http://localhost:3000');
+});
